a6e78e8fd579d96fc5d6b37b26916f7dea299c9a
Martemianjcheva A <d.shmakov1001@gmail.com>
2025-02-06 19:09:44 +0300

    fix ; merge back and front

--- backend-server/interface/token.ts
+++
@@ -1,6 +0,0 @@
-export interface IToken {
-    id: string;
-    user_id: string;
-    refresh_token: string;
-    created_at?: string;
-}
--- backend-server/interface/user.ts
+++
@@ -1,8 +0,0 @@
-export interface IUser {
-    id: string;
-    email: string;
-    password: string;
-    is_activated: boolean;
-    activation_link: string;
-    created_at?: string;  // Автоматически добавляется Supabase
-}
