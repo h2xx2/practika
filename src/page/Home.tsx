e280127f214b13707f6cff60ced35f0c7ca0af8b
shmakov daniil <d.shmakov1001@gmail.com>
2025-02-18 11:04:35 +0300

    feat : add chat

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -2,11 +2,14 @@ import DrawerAppBar from "../components/NavigationHeader.tsx";
 import Main from "../components/Main/Main.tsx";
 import Footer from "../components/footer/Footer.tsx";
 
+import ChatWidget from "../components/Chat/Chat.tsx";
+
 export default function Home(){
     return (
         <>
             <DrawerAppBar></DrawerAppBar>
             <Main></Main>
+            <ChatWidget></ChatWidget>
             <Footer></Footer>
         </>
     )
